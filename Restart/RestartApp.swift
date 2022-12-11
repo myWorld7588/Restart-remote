0d041710c238b991cb147e47d2a5e17f924c8ebe
myWorld7588 <jin.jakechoi@gmail.com>
2022-12-11 13:58:46 -0500

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Jake Choi on 12/11/22.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
