12265c7333046bfdf21259f3e572d97bc0b8a27f
myWorld7588 <jin.jakechoi@gmail.com>
2022-12-11 21:15:48 -0500

    Drag Gesture 1. Drag and swipe the button in the right direction 2. Button Shape expand horizontally 3. Button snapped back to position when its half way 4. Button trigger switch when drag till end & screen change
    **Property represent the offset value in the horizontal direction, Button has two states.**
    
    1. active state when user are dragging
    2. Idle state when button is inactive
    
    Initialize offset size with a zero value and offset value will constantly change during the dragging activity
    
    keeps button in position inside the component regarding the horizontal directions with - 80
    
    Red button in position zero when stop dragging.

--- Restart/Screens/OnboardingView.swift
+++ Restart/Screens/OnboardingView.swift
@@ -13,6 +13,10 @@ struct OnboardingView: View {
     
     @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
     
+    @State private var buttonWidth: Double = UIScreen.main.bounds.width - 80
+    @State private var buttonOffset: CGFloat = 0
+    
+    
     
     // MARK: - BODY
     
@@ -87,7 +91,7 @@ struct OnboardingView: View {
                     HStack {
                         Capsule()
                             .fill(Color("ColorRed"))
-                            .frame(width: 80)
+                            .frame(width: buttonOffset + 80)
                         Spacer()
                     }
                     // 4. CIRCLE (DRAGGABLE)
@@ -105,16 +109,30 @@ struct OnboardingView: View {
                         }
                         .foregroundColor(.white)
                     .frame(width: 80, height: 80, alignment: .center)
-                    .onTapGesture {
-                        isOnboardingViewActive = false
-                    }
+                    .offset(x: buttonOffset)
+                    .gesture(DragGesture()
+                        .onChanged { gesture in
+                            if gesture.translation.width > 0  && buttonOffset <= buttonWidth - 80 {
+                                buttonOffset = gesture.translation.width
+                                
+                            }
+                        }
+                        .onEnded { _ in
+                            buttonOffset = 0
+                            if buttonOffset > buttonWidth / 2 {
+                                buttonOffset = buttonWidth - 80
+                                isOnboardingViewActive = false
+                            } else {
+                                buttonOffset = 0
+                            }
+                        }
+                    
+                    ) //: GESTURE
                         
                         Spacer()
                     } //: HSTACK
-                    
-                    
                 } //: FOOTER
-                .frame(height: 80, alignment: .center)
+                .frame(width: buttonWidth, height: 80, alignment: .center)
                 .padding()
             } //: VSTACK
         } //: ZSTACK
