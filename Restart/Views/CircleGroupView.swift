910f33e474bd13c72cd2531399555b080ec76b89
myWorld7588 <jin.jakechoi@gmail.com>
2022-12-11 19:41:34 -0500

    Complete design Home Screen, Create a reusable UI Component .buttonStyle() let you create button style Create CircleGroupView (Reusable) Can reuse `CircleGroupView(ShapeColor: .white, ShapeOpacity: 0.2)` on other view reusable user interface component with variable attributes.

---
+++ Restart/Views/CircleGroupView.swift
@@ -0,0 +1,45 @@
+//
+//  CircleGroupView.swift
+//  Restart
+//
+//  Created by Jake Choi on 12/11/22.
+//
+
+import SwiftUI
+
+struct CircleGroupView: View {
+    // MARK: - PROPERTY
+    
+    
+    
+    @State var ShapeColor: Color
+    @State var ShapeOpacity: Double
+    
+    
+    // MARK: - BODY
+    
+    var body: some View {
+        ZStack {
+            Circle()
+                .stroke(ShapeColor.opacity(ShapeOpacity), lineWidth: 40)
+                .frame(width: 260, height: 260, alignment: .center)
+            Circle()
+                .stroke(ShapeColor.opacity(ShapeOpacity), lineWidth: 80)
+                .frame(width: 260, height: 260, alignment: .center)
+        } //: ZSTACK
+    }
+}
+
+// MARK: - PREVIEW
+
+
+struct CircleGroupView_Previews: PreviewProvider {
+    static var previews: some View {
+        ZStack {
+            Color("ColorBlue")
+                .ignoresSafeArea(.all, edges: .all)
+            
+            CircleGroupView(ShapeColor: .white, ShapeOpacity: 0.2)
+        }
+    }
+}
