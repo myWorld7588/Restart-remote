044a85779e627d1371dfa8c457ccefc5c16dd854
myWorld7588 <jin.jakechoi@gmail.com>
2022-12-12 21:21:28 -0500

    create drag gesture @State private var imageOffset: CGSize = .zero // imageOffset 값을 새 속성과 함께 저장

--- Restart/Screens/OnboardingView.swift
+++ Restart/Screens/OnboardingView.swift
@@ -16,6 +16,7 @@ struct OnboardingView: View {
     @State private var buttonWidth: Double = UIScreen.main.bounds.width - 80
     @State private var buttonOffset: CGFloat = 0
     @State private var isAnimating: Bool = false
+    @State private var imageOffset: CGSize = .zero
     
     
     
@@ -64,6 +65,15 @@ struct OnboardingView: View {
                         .scaledToFit()
                         .opacity(isAnimating ? 1 : 0)
                         .animation(.easeOut(duration: 0.5), value: isAnimating)
+                        .offset(x: imageOffset.width * 1.2, y: 0)
+                        .gesture(
+                            DragGesture()
+                                .onChanged { gesture in
+                                    imageOffset = gesture.translation
+                                    
+                                }
+                        
+                        ) //: GESTURE
                 } //: CENTER
                 
                 Spacer()
