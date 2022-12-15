1fb324b1327aa915c8c47329c22659b1539c6bae
myWorld7588 <jin.jakechoi@gmail.com>
2022-12-15 00:09:34 -0500

    #Haptic Feedback let hapticFeedback = UINotificationFeedbackGenerator() - Will use this property to trigger a notification feedback by calling its particular built in method.
    hapticFeedback.notificationOccurred(.success)
    hapticFeedback.notificationOccurred(.warning)
    - This notification occurred method tells the generator that a task or action has succeeded

--- Restart/Screens/OnboardingView.swift
+++ Restart/Screens/OnboardingView.swift
@@ -20,6 +20,8 @@ struct OnboardingView: View {
     @State private var indicatorOpacity: Double = 1.0
     @State private var textTitle: String = "Share."
     
+    let hapticFeedback = UINotificationFeedbackGenerator()
+    
     
     
     
@@ -173,14 +175,18 @@ struct OnboardingView: View {
                         .onEnded { _ in
                             withAnimation(Animation.easeOut(duration: 0.4)) {
                                 if buttonOffset > buttonWidth / 2 {
+                                    hapticFeedback.notificationOccurred(.success)
                                     playSound(sound: "chimeup", type: "mp3")
                                     buttonOffset = buttonWidth - 80
                                     isOnboardingViewActive = false
                                 } else {
+                                    hapticFeedback.notificationOccurred(.warning)
                                     buttonOffset = 0
                                 }
                             }
                         }
+                             
+                             
                     
                     ) //: GESTURE
                         
